7344f5b4b8f48de6475133660f95362353df9e32
kos-maj <kosmajac@ualberta.ca>
2022-05-20 13:05:32 -0600

    up

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
-chrome.runtime.onInstalled.addListener(async () => {
-    let url = chrome.runtime.getURL("youtube.com");
+chrome.browserAction.onClicked.addListener(async () => {
+    let url = "https://youtube.com/"
 
     let tab = await chrome.tabs.create({url});
     console.log(`Created tab ${tab.id}.`);

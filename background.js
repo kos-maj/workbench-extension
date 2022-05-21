4ca6905153b93f2844b68118f1fd31caa9ec3f05
kos-maj <kosmajac@ualberta.ca>
2022-05-20 18:03:34 -0600

    stage 1 semi-complete, need to figure out hosting front-end

--- background.js
+++ background.js
@@ -1,7 +1,34 @@
 
 console.log("[+] service running...");
 
-// Listener for message sent from content script
+const injectedScript = url => {
+    if (window.location.href.includes('youtube.com')) {
+        const extractedURL = url;
+        document.querySelector('input').value = extractedURL;
+    }
+}
+
+async function createNewTab(url) {
+    let tab = await chrome.tabs.create({ url });
+    return tab;
+}
+
 chrome.runtime.onMessage.addListener( (request) => {
-    console.log(request);
+    const url   = "https://youtube.com";
+    const exurl = request;
+    console.log(exurl);
+
+    createNewTab(url).then(tab => {
+        if(typeof tab !== 'undefined') {
+            console.log(`[+] Injecting script into tabid = '${tab.id}'\nexurl = ${exurl}...`);
+            chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                function: injectedScript,
+                args: [exurl]
+            })
+        } else {
+            console.log("Error: could not create new tab - chrome.tabs.create() returned undefined.");
+        }
+    })
+
 })
--- popup.js
+++ popup.js
@@ -4,14 +4,11 @@ btn.addEventListener("click", async () => {
     const url = "https://youtube.com/";
 
     try {
-        // Get currently active tab URL
+        // Send currently active tab to background service
         let tabQuery = await chrome.tabs.query({active: true, currentWindow: true});
-        const exURL = tabQuery[0].url;
-        
-        // Create new tab and send information to background service
-        let tab = await chrome.tabs.create({url, active: false}, tab => {
-            chrome.runtime.sendMessage({ exURL: exURL, tab: tab })
-        });
-    } catch (err) { msg = err };
+        chrome.runtime.sendMessage(tabQuery[0].url)
+    } catch (err) { 
+        chrome.runtime.sendMessage(err);
+    }
    
-})
+});

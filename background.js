b0f44ceea84b7f6a0a98fb8c547572f0ac0a0341
kos-maj <kosmajac@ualberta.ca>
2022-05-24 16:32:40 -0600

    swapped promise with async...await on message listener

--- background.js
+++ background.js
@@ -3,32 +3,37 @@ console.log("[+] service running...");
 
 const injectedScript = url => {
     if (window.location.href.includes('youtube.com')) {
-        const extractedURL = url;
-        document.querySelector('input').value = extractedURL;
+        document.querySelector('input').value = url;
+        // TO DO: click button after tab loads? 
+        // const btn = document.getElementById('button');
     }
 }
 
-async function createNewTab(url) {
+chrome.runtime.onMessage.addListener(async (previousURL) => {
+    const url   = 'https://youtube.com';
     let tab = await chrome.tabs.create({ url });
-    return tab;
-}
 
-chrome.runtime.onMessage.addListener( (request) => {
-    const url   = "https://youtube.com";
-    const exurl = request;
-    console.log(exurl);
-
-    createNewTab(url).then(tab => {
-        if(typeof tab !== 'undefined') {
-            console.log(`[+] Injecting script into tabid = '${tab.id}'\nexurl = ${exurl}...`);
-            chrome.scripting.executeScript({
-                target: { tabId: tab.id },
-                function: injectedScript,
-                args: [exurl]
-            })
-        } else {
-            console.log("Error: could not create new tab - chrome.tabs.create() returned undefined.");
-        }
-    })
+    if(typeof tab  !== 'undefined') {
+        console.log(`[+] Injecting script into tabid = '${tab.id}'\nprev_url = '${previousURL}`);
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: injectedScript,
+            args: [previousURL]
+        });
+    } else {
+        console.log("Error: could not create new tab - chrome.tabs.create() returned undefined.");
+    }
+    // createNewTab(url).then(tab => {
+        // if(typeof tab !== 'undefined') {
+            // console.log(`[+] Injecting script into tabid = '${tab.id}'\nexurl = ${previousURL}...`);
+            // chrome.scripting.executeScript({
+                // target: { tabId: tab.id },
+                // function: injectedScript,
+                // args: [previousURL]
+            // })
+        // } else {
+            // console.log("Error: could not create new tab - chrome.tabs.create() returned undefined.");
+        // }
+    // })
 
 })

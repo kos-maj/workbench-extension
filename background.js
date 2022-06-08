22f169e4092edaa7da4a2d016364ee37a5293dc2
kos-maj <kosmajac@ualberta.ca>
2022-06-08 14:17:05 -0600

    changed tests - user input still not registered

--- background.js
+++ background.js
@@ -4,7 +4,6 @@ let g_url;
 let g_pressed = false;
 
 const injectedScript = function (prev_url) {
-    const url       = String(prev_url);
     const userInput = document.querySelector('input');
     const loadNews  = document.querySelectorAll('button').item(0);
     const lucky     = document.querySelectorAll('button').item(1);
@@ -12,28 +11,34 @@ const injectedScript = function (prev_url) {
     // Focus on input and clear field
     userInput.select();
     userInput.value = '';
+
+    userInput.value = prev_url;
+    loadNews.click();
     
+    // TODO: fix frontend not registering user input??
+
     // ((((Testing))))
-    let current = 0;
-    let len = url.length;
-
-    const writeURL = () => {
-        if(current < len-1) {
-            userInput.value += url[current];
-            current++;
-            setTimeout(() => {
-                writeURL();
-            }, 80);
-        } else {
-            loadNews.click();
-            setTimeout(() => {userInput.value += 'END';}, 1000);
-        }
-    }
-
-    writeURL();
-    
-    // Wait for RUN NER button to appear, then press?
+    // let current = 0;
+    // let len = url.length;
  
+    // const writeURL = () => {
+        // if(current < len-1) {
+            // userInput.value += url[current];
+            // current++;
+            // setTimeout(() => {
+                // writeURL();
+            // }, 80);
+        // } else {
+            // loadNews.click();
+            // setTimeout(() => {userInput.value += 'END';}, 1000);
+        // }
+    // }
+ 
+    // writeURL();
+    
+
+    
+    // TODO: wait for RUN NER button to appear, then press 
 };
 
 chrome.runtime.onMessage.addListener((previousURL) => {

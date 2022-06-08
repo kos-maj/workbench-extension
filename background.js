c29f6f43b85105cf832e2a541bbdd22d0167788c
kos-maj <kosmajac@ualberta.ca>
2022-06-08 14:14:46 -0600

    Routes to workbench frontend - still needs fixing -> does not register user input

--- background.js
+++ background.js
@@ -4,19 +4,43 @@ let g_url;
 let g_pressed = false;
 
 const injectedScript = function (prev_url) {
-    if (window.location.href.includes('youtube.com')) {
-        document.querySelector('input').setAttribute('value', prev_url);
+    const url       = String(prev_url);
+    const userInput = document.querySelector('input');
+    const loadNews  = document.querySelectorAll('button').item(0);
+    const lucky     = document.querySelectorAll('button').item(1);
 
-        // TO DO: click button after tab loads? 
-        // const btn = document.getElementById('button');
+    // Focus on input and clear field
+    userInput.select();
+    userInput.value = '';
+    
+    // ((((Testing))))
+    let current = 0;
+    let len = url.length;
+
+    const writeURL = () => {
+        if(current < len-1) {
+            userInput.value += url[current];
+            current++;
+            setTimeout(() => {
+                writeURL();
+            }, 80);
+        } else {
+            loadNews.click();
+            setTimeout(() => {userInput.value += 'END';}, 1000);
+        }
     }
+
+    writeURL();
+    
+    // Wait for RUN NER button to appear, then press?
+ 
 };
 
 chrome.runtime.onMessage.addListener((previousURL) => {
     // Get url, set pressed to true, and create a new tab
     g_url     = previousURL;
     g_pressed = true;
-    const url = 'https://youtube.com';
+    const url = 'https://newskg.wdmuofa.ca/';
 
     chrome.tabs.create({ url });
 });
@@ -24,6 +48,7 @@ chrome.runtime.onMessage.addListener((previousURL) => {
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     // Only execute script if updated tab is a result of the extension button press
     if(tab.status === 'complete' && g_pressed) {
+        console.log(`len: ${g_url.length}\nurl: ${g_url}`);
         g_pressed = false;
         chrome.scripting.executeScript({
             target: {tabId: tab.id},

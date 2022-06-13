5cbf44afaf7607e9c4233a3ac4d59a83aff4cf12
kos-maj <kosmajac@ualberta.ca>
2022-06-13 13:17:26 -0600

    Testing search functionality on google images - functions fine

--- background.js
+++ background.js
@@ -4,28 +4,19 @@ let g_url;
 let g_pressed = false;
 
 const injectedScript = function (prev_url) {
+    // const userInput = document.querySelector('input');
+    // const loadNews  = document.querySelectorAll('button').item(0);
+    // const lucky     = document.querySelectorAll('button').item(1);
+
     const userInput = document.querySelector('input');
-    const loadNews  = document.querySelectorAll('button').item(0);
-    const lucky     = document.querySelectorAll('button').item(1);
+    const search = document.querySelectorAll('button').item(1);
 
     // Focus on input and paste url
     userInput.select();
     userInput.value = prev_url;
-    
-    // loadNews.click();
+    search.click(); 
 
-    // TODO: fix frontend not registering user input??
-    // userInput.dispatchEvent(new KeyboardEvent("keydown", {
-        // "key": "b"
-    // }));
-    // userInput.dispatchEvent(new KeyboardEvent("keyup", {
-        // "key": "b"
-    // }));
 
-    setTimeout(() => { loadNews.click(); }, 500);
-
-   
-    
     /** ((TESTING)) */
     // let current = 0;
     // let len = prev_url.length;
@@ -51,7 +42,8 @@ chrome.runtime.onMessage.addListener((previousURL) => {
     // Get url, set pressed to true, and create a new tab
     g_url     = previousURL;
     g_pressed = true;
-    const url = 'https://newskg.wdmuofa.ca/';
+    // const url = 'https://newskg.wdmuofa.ca/';
+    const url = 'https://images.google.ca';
 
     chrome.tabs.create({ url });
 });
@@ -59,7 +51,6 @@ chrome.runtime.onMessage.addListener((previousURL) => {
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     // Only execute script if updated tab is a result of the extension button press
     if(tab.status === 'complete' && g_pressed) {
-        console.log(`len: ${g_url.length}\nurl: ${g_url}`);
         g_pressed = false;
         chrome.scripting.executeScript({
             target: {tabId: tab.id},
